ab91ccc8f59c8010d5f260d6814c811a5726acda
0hyunCho <yhcho.inspien@co.kr>
2024-10-22 22:48:18 +0900

    fixed

--- src/main/java/com/example/elice/TestService.java
+++ src/main/java/com/example/elice/TestService.java
@@ -1,11 +1,13 @@
 package com.example.elice;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class TestService {
 
     private TestRepository testRepository;
