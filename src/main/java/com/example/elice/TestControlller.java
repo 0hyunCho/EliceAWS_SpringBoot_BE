69b7d60f4cf87e630b0208d6d954fe6704aea0f2
0hyunCho <yhcho.inspien@co.kr>
2024-10-22 22:25:33 +0900

    initiate project

---
+++ src/main/java/com/example/elice/TestControlller.java
@@ -0,0 +1,28 @@
+package com.example.elice;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class TestControlller {
+
+    private TestService testService;
+
+    @GetMapping
+    public ResponseEntity<List<String>> getAllContents() {
+        return ResponseEntity.ok(testService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<TestEntity> postContent(@RequestBody String content) {
+        return ResponseEntity.ok(testService.createContent(content));
+    }
+}
