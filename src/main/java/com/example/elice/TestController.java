b8df618f2060904f78c8e57572f6eab764dba05c
0hyunCho <yhcho.inspien@co.kr>
2024-10-23 22:06:17 +0900

    test

--- src/main/java/com/example/elice/TestController.java
+++ src/main/java/com/example/elice/TestController.java
@@ -1,8 +1,14 @@
 package com.example.elice;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -15,13 +21,19 @@ public class TestController {
         return "Hello Elice";
     }
 
-//    @GetMapping("/content")
-//    public ResponseEntity<List<String>> getAllContents() {
+    @GetMapping("/content")
+    public ResponseEntity<List<String>> getAllContents() {
+        List<String> contents = new ArrayList<>();
+        contents.add("Hello Elice");
+        contents.add("Hello World");
+        return ResponseEntity.ok(contents);
 //        return ResponseEntity.ok(testService.findAll());
-//    }
+    }
 //
-//    @PostMapping("/content")
+    @PostMapping("/content")
 //    public ResponseEntity<TestEntity> postContent(@RequestBody String content) {
+    public ResponseEntity<String> postContent(@RequestBody String content) { // 이후 삭제 필요
+        return ResponseEntity.ok("POSTED : " + content);
 //        return ResponseEntity.ok(testService.createContent(content));
-//    }
+    }
 }
